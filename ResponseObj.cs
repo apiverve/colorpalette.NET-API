04569e4e5e298ee2fbc32cdeff655ab2e3645797
APIVerve Team <hello@apiverve.com>
2024-05-14 15:22:45 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,45 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class colorPalette
+{
+    [JsonProperty("hex")]
+    public string hex { get; set; }
+
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("source")]
+    public string source { get; set; }
+
+    [JsonProperty("hue")]
+    public int hue { get; set; }
+
+    [JsonProperty("colorPalette")]
+    public colorPalette[] colorPalette { get; set; }
+
+    [JsonProperty("colorPaletteRaw")]
+    public string[] colorPaletteRaw { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
