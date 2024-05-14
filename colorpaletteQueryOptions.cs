a94b3fdeda08bca2de9cffbc943458fd4e7b63f3
APIVerve Team <hello@apiverve.com>
2024-05-14 15:22:42 -0500

    Bug fixes and Improvements

---
+++ colorpaletteQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class colorpaletteQueryOptions {
+public string color { get; set; }
+public string variation { get; set; }
+}
+
+}
