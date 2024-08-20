8726a673a7d7a54d668630eadf2f055668e1df2b
APIVerve Team <hello@apiverve.com>
2024-08-20 10:46:18 -0500

    Bug Fixes

---
+++ ColorPaletteGeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ColorPaletteGeneratorQueryOptions {
+public string color { get; set; }
+public string variation { get; set; }
+}
+
+}
